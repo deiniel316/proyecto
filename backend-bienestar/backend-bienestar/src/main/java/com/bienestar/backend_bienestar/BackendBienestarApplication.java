7467d956493e351c3e8b6f835fd722c0934284eb
Daniel Guamuch <danielguamuch512@gmail.com>
2025-09-12 22:50:41 -0600

    commit del proyecto

---
+++ backend-bienestar/backend-bienestar/src/main/java/com/bienestar/backend_bienestar/BackendBienestarApplication.java
@@ -0,0 +1,13 @@
+package com.bienestar.backend_bienestar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendBienestarApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackendBienestarApplication.class, args);
+	}
+
+}
---
+++ backend-bienestar/backend-bienestar/src/test/java/com/bienestar/backend_bienestar/BackendBienestarApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bienestar.backend_bienestar;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendBienestarApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
